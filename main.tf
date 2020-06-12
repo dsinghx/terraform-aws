26697a4e2af9e12a4fa2b1f45dac1c591fec6ac8
Dalvir Singh <dsingh@deloitte.com.au>
2020-06-12 17:37:29 +1000

    added ec2 instance

--- main.tf
+++ main.tf
@@ -11,3 +11,20 @@ resource "aws_vpc" "example_VPC" {
       Name = "vpc_with_a_name"
   }
 }
+
+# provision an ec2 instance
+
+resource "aws_instance" "example_instance" {
+  ami = "ami-04fcc97b5f6edcd89"
+  instance_type = "t2.micro"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+  
+  tags = {
+    Name = "terraform-example"
+  }
+}
